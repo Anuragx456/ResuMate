b9dd2f326a01d0f072beb036ecb881e9ac3d2dc5
Anuragx456 <anuragcolab0@gmail.com>
2026-01-29 11:20:58 +0530

    Resume Feedback Page

--- app/routes.ts
+++ app/routes.ts
@@ -4,6 +4,6 @@ export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/Auth.tsx'),
     route('/upload', 'routes/Upload.tsx'),
-    // route('/resume/:id', 'routes/resume.tsx'),
+    route('/resume/:id', 'routes/Resume.tsx'),
     // route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
